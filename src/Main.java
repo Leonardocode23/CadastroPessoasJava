e48e3b91c364267a0f5624b06f25cf8ce8ec76a9
Leonardo-Santos <leonardo.co.de23@gmail.com>
2023-10-05 21:44:15 -0300

    inicio do projeto

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Bem vindo ao sistema de cadastro de Pessoa Fisica e Pessoa Jurítica!!!");
+
+        Scanner scanner = new Scanner(System.in);
+    }
+}
---
+++ src/Pessoa.java
@@ -0,0 +1,10 @@
+public class Pessoa {
+    public String nome;
+    public int rendimento;
+
+    public Endereco endereco;
+
+    public float CalcularImposto(int rendimento) {
+        return rendimento;
+    }
+}
